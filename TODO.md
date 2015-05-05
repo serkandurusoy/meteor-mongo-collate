aaa717ee6b0f59fe08f52c6d5d51253573dd7feb
serkandurusoy <serkan.durusoy@dna-tr.com>
2015-05-05 14:25:59 +0300

    initial implementation of insert with some basic checks

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 - [ ] Spread the word and get contributors on board, perhaps hand this over to
       one of those more organized meteor i18n initiatives
 - [ ] Write some tests
+- [ ] Make sure it works with simpleschema
 - [ ] Hide on the client if there is no collation data
 - [ ] Apply default sorting
 - [ ] Create and export a utility function that collates given document(s)
--- mongo-collate.js
+++ mongo-collate.js
@@ -1,19 +1,89 @@
+var bin2hex = function (s) {
+  //  discuss at: http://phpjs.org/functions/bin2hex/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // bugfixed by: Onno Marsman
+  // bugfixed by: Linuxworld
+  // improved by: ntoniazzi (http://phpjs.org/functions/bin2hex:361#comment_177616)
+  //   example 1: bin2hex('Kev');
+  //   returns 1: '4b6576'
+  //   example 2: bin2hex(String.fromCharCode(0x00));
+  //   returns 2: '00'
+
+  var i, l, o = '',
+      n;
+
+  s += '';
+
+  for (i = 0, l = s.length; i < l; i++) {
+    n = s.charCodeAt(i)
+        .toString(16);
+    o += n.length < 2 ? '0' + n : n;
+  }
+
+  return o;
+};
+
+
 if (Meteor.isServer) {
 
-  Mongo.Collection.prototype.collate = function (options) {
-    var self    = this,
-        keys    = options.keys,
-        locales = options.locales;
+  Mongo.Collection.prototype.collate = function(options) {
+    var self = this;
+    var keys;
+    var locales;
+    var store;
 
-    self.before.insert(function (userId, doc) {
-      doc.collators = {};
-      _.each(keys, function (key) {
-        doc.collators[key] = {};
-        _.each(locales, function (locale) {
-          var collator = new ilib.Collator({locale: locale});
-          doc.collators[key][locale] = collator.sortKey(doc[key]);
-        });
+    check(options, {
+      keys: [String],
+      locales: [String],
+      store: Match.Optional(String),
+      precision: Match.Optional(Match.Integer)
+    });
+
+    keys = options.keys;
+    locales = options.locales;
+    store = options.store || 'collators';
+    precision = options.precision || 5;
+
+    var availableLocales = [/* ilib.Locale.getAvailableLocales() */];
+
+    _.each(locales, function(locale) {
+      if (!_.contains(availableLocales, locale)) {
+        throw new Meteor.Error('locale-not-available',
+            'Requested locale '
+            + locale
+            + ' is either incorrect or not available');
+      }
+    });
+
+    if (precision <= 0) {
+      throw new Meteor.Error('precision-incorrect',
+          'Precision must be greater than zero');
+    }
+
+    self.before.insert(function(userId, doc) {
+      if (doc[store]) {
+        throw new Meteor.Error('store-not-available',
+            'The document cannot contain a key named ' + store);
+      }
+
+      doc[store] = {};
+      
+      _.each(keys, function(key) {
+        if (!doc[key]) {
+          console.warn('The document does not contain a collatible key named '
+              + key
+              + '. Your sorts on this key may be skewed.');
+        } else {
+          doc[store][key] = {};
+
+          _.each(locales, function(locale) {
+            var collator = new ilib.Collator({locale: locale});
+            doc.collators[key][locale] = bin2hex(collator.sortKey(doc[key].substr(0, precision)));
+          });
+        }
+
       });
+      
     });
 
   };
